bcd36731043158090d484cd26d25e7608f98f6f3
Marco <kuvimarco@gmail.com>
2020-08-27 10:26:30 +0200

    Add SimpleCalculations

---
+++ src/SimpleCalculations.java
@@ -0,0 +1,21 @@
+public class SimpleCalculations {
+    public static void main (String [] args) {
+        int a = 27;
+        int b = 45;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+
+        double c = 23.5;
+        double d = 36.7;
+
+        System.out.println(c + d);
+        System.out.println(Math.round((c - d) * 100) /100);
+        System.out.println(c * d);
+        System.out.println(Math.round((c / d) * 100) /100);
+
+
+    }
+}
